f815587a0ba08065799578b9ca09f53718872e95
Lu Yihui <lu_yihui@126.com>
2019-11-24 18:12:58 +0800

    add login page

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ var query = require('../api/query');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'ExpressTitle' });
+    if(req.query.password == 'sjtu123'){
+      res.render('index.html', { title: 'ExpressTitle' });
+      
+    }else{
+    res.render('login.html', { title: 'ExpressTitle' });
+    }
+});
+
+router.get('/login', function(req, res, next) {
+  if(req.query.password == 'sjtu123'){
+    res.status(200).send({token:'hello'});
+  }else{
+    res.status(666).send('wrong');
+  }
+});
+
+router.get('/home', function(req, res, next) {
+  if(req.query.token == 'hello'){
+    res.render('index.html', { title: 'ExpressTitle' });
+  }else{
+    res.render('error.html', { title: 'ExpressTitle' });
+  }
 });
 
 
@@ -24,7 +45,7 @@ router.get('/query/all/all', query.get_all_all);
 
 /**请求所有车辆的历史状态（计算多级折线图）
  * 示例 http://202.120.60.31:3000/query/history/status?limit=5000000
- * 参数 limit = [1,1000] -> 按分钟为单位，距今的分钟数
+ * 参数 limit = [1,1000] -> 按分钟为单位，距今的分钟数 默认为100
  * res = [{
  *      car_VIN,
  *      latest_info:{
@@ -63,7 +84,7 @@ router.get('/query/all/status', query.get_all_status);
 /**请求单车当前所有信息
  * 参数 car_VIN = "LXXXXXXXXXXXXXXXXX"
  * res = {
- *      speed = 20
+ *      velocity = 20
  *      drive_motor_rpm = 30
  *      ...
  * }
@@ -73,10 +94,10 @@ router.get('/query/single/info', query.get_single_info);
 /**请求单车某项历史记录
  * 参数 car_VIN = "LXXXXXXXXXXXXXXXXX"
  * 参数 type = {velocity, drive_motor_rpm, drive_motor_temperature ...}
- * 参数 length = [1,1000]
+ * 参数 length = [1,1000] -> 按分钟为单位，距今的分钟数 默认为100
  * res = [{
  *      create_time = ...
- *      speed = 20
+ *      velocity = 20
  *      drive_motor_rpm = 30
  *      ...
  * },{}...]
@@ -85,7 +106,7 @@ router.get('/query/single/history', query.get_single_history);
 
 /**请求单车历史轨迹
  * 参数 car_VIN = "LXXXXXXXXXXXXXXXXX"
- * 参数 length = [1,1000]
+ * 参数 length = [1,1000] -> 按分钟为单位，距今的分钟数 默认为100
  * res = [{
  *      create_time = ...
  *      longitude = 20
